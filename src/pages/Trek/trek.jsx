5c49b8734b43eef0f4aa3b337a6441eddfb37b08
kshitiz-kothari31 <kshitizkothari69@gmail.com>
2025-07-06 17:45:27 +0530

    90% completed the Trek Main Page

--- src/pages/Trek/trek.jsx
+++ src/pages/Trek/trek.jsx
@@ -1,7 +1,13 @@
 // INFO: Images imports
 import heroImg from "../../assets/images/TrekMain/heroImage.png";
-import Trek1 from "../../assets/images/TrekMain/DayaraBuyalTrek.jpg"
+import Trek1 from "../../assets/images/TrekMain/DayaraBuyalTrek.jpg";
 import downArrow from "../../assets/downArrow.png";
+import leftBtn from "../../assets/leftArrow.png";
+import rightBtn from "../../assets/rightArrow.png";
+import GuideImg1 from "../../assets/images/guidesImage/1.jpg"
+import GuideImg2 from "../../assets/images/guidesImage/2.jpg"
+import GuideImg3 from "../../assets/images/guidesImage/3.jpg"
+import GuideImg4 from "../../assets/images/guidesImage/4.jpg"
 import styles from "./trek.module.css";
 
 function Trek() {
@@ -33,7 +39,6 @@ function Trek() {
         </p>
       </div>
 
-
       <div className={styles.Trek1}>
         <div className={styles.images}>
           <img className={styles.img1} src={Trek1} alt="This is an image!" />
@@ -45,11 +50,12 @@ function Trek() {
             Discover epic trails, expert tips, and inspiring stories to fuel
             your next moutain adventure.
           </p>
-          <p><b>Cost: ₹7,999 /-</b></p>
+          <p>
+            <b>Cost: ₹7,999 /-</b>
+          </p>
         </div>
         <button className={styles.details}>More Details</button>
       </div>
-
 
       <div className={styles.Trek2}>
         <div className={styles.images}>
@@ -62,11 +68,12 @@ function Trek() {
             Discover epic trails, expert tips, and inspiring stories to fuel
             your next moutain adventure.
           </p>
-          <p><b>Cost: ₹8,999 /-</b></p>
+          <p>
+            <b>Cost: ₹8,999 /-</b>
+          </p>
         </div>
         <button className={styles.details2}>More Details</button>
       </div>
-
 
       <div className={styles.Trek1}>
         <div className={styles.images}>
@@ -78,11 +85,12 @@ function Trek() {
           <p>
             Discover epic trails, expert tips, and inspiring stories to fuel
           </p>
-          <p><b>Cost: ₹14,999 /-</b></p>
+          <p>
+            <b>Cost: ₹14,999 /-</b>
+          </p>
         </div>
         <button className={styles.details}>More Details</button>
       </div>
-
 
       <div className={styles.Trek2}>
         <div className={styles.images}>
@@ -94,12 +102,13 @@ function Trek() {
           <p>
             Discover epic trails, expert tips, and inspiring stories to fuel
           </p>
-          <p><b>Cost: ₹2,500 /-</b></p>
+          <p>
+            <b>Cost: ₹2,500 /-</b>
+          </p>
         </div>
         <button className={styles.details2}>More Details</button>
       </div>
 
-      
       <div className={styles.Trek1}>
         <div className={styles.images}>
           <img className={styles.img9} src={Trek1} alt="This is an image!" />
@@ -111,9 +120,27 @@ function Trek() {
             Discover epic trails, expert tips, and inspiring stories to fuel
             your next moutain adventure.
           </p>
-          <p><b>Cost: ₹5,999 /-</b></p>
+          <p>
+            <b>Cost: ₹5,999 /-</b>
+          </p>
         </div>
         <button className={styles.details}>More Details</button>
+      </div>
+
+      <div className={styles.ImageSection}>
+        <h2>Your Guides Your Travel Partners</h2>
+        <div className={styles.slidingImages}>
+          <img className={styles.leftBtn} src={leftBtn} />
+          <div className={styles.slideImages}>
+            <img className={styles.GuideImg1} src={GuideImg1} />
+            <img className={styles.GuideImg2} src={GuideImg2} />
+            <img className={styles.GuideImg1} src={GuideImg3} />
+            <img className={styles.GuideImg2} src={GuideImg4} />
+          </div>
+          <img className={styles.rightBtn} src={rightBtn} alt="" />
+        </div>
+        <p>Our expert guides are the heart of every journey we offer—passionate storytellers, seasoned explorers, and friendly companions rolled into one. With deep local knowledge and a genuine love for discovery, they turn every trip into a rich, immersive experience. Whether you're navigating ancient trails, bustling cities, or remote landscapes, our guides ensure you see the unseen and feel the spirit of every place you visit.</p>
+        <button>About Guides</button>
       </div>
     </div>
   );
