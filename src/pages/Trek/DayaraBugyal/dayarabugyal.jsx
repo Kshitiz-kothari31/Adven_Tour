07dbcdf3d50429d6c9061a6df2f5b8fbe3bde57c
kshitiz-kothari31 <kshitizkothari69@gmail.com>
2025-07-10 23:53:10 +0530

    Completed the DayaraBuyal Trek Page but without animations

--- src/pages/Trek/DayaraBugyal/dayarabugyal.jsx
+++ src/pages/Trek/DayaraBugyal/dayarabugyal.jsx
@@ -1,10 +1,12 @@
+import { style } from "framer-motion/client";
 import dropdownImg from "../../../assets/downArrow.png";
 import img from "../../../assets/images/TrekMain/DayaraBuyalTrek.jpg";
+import img2 from "../../../assets/images/TrekMain/img2.svg";
 import styles from "./dayarabugyal.module.css";
 
 function DayaraBugyal() {
   return (
-    <>
+    <main>
       <section className={styles.firstSection}>
         <h1>Dayara Buyal Trek</h1>
         <button>Book Now</button>
@@ -48,7 +50,132 @@ function DayaraBugyal() {
           </p>
         </div>
       </section>
-    </>
+
+      <section className={styles.storySection}>
+        <h2>
+          The Dayara Bugyal trek isn’t just a walk through meadows and
+          mountains; it’s a journey steeped in mythology, folklore, and quiet
+          reverence.
+        </h2>
+        <p>
+          <b>Sages and Seekers: </b> According to local lore, Dayara Bugyal was
+          once a sacred retreat for ancient sages. They meditated in these
+          serene meadows, believing the high-altitude silence brought them
+          closer to enlightenment.
+        </p>
+        <hr />
+        <p>
+          <b>Lord Shiva’s Blessing: </b> Some legends say that Lord Shiva
+          himself wandered through these lands. The peaceful energy and
+          spiritual aura are said to be remnants of his presence.
+        </p>
+        <hr />
+        <p>
+          <b>The Pandavas’ Path: </b> The Mahabharata weaves its way into the
+          story too. It’s believed that the Pandavas, during their exile, passed
+          through Dayara Bugyal and performed rituals here. That mystical
+          energy? Locals say it still lingers in the air.
+        </p>
+      </section>
+
+      <section className={styles.daysSection}>
+        <div className={styles.dayBox}>
+          <h2>Day x to y on the trek</h2>
+          <p>
+            According to local lore, Dayara Bugyal was once a sacred retreat for
+            ancient sages. They meditated in these serene meadows, believing the
+            high-altitude silence brought them closer to enlightenment.{" "}
+          </p>
+          <div className={styles.imgWithDetail}>
+            <div className={styles.imgBox}>
+              <div className={styles.firstimg}></div>
+              <div className={styles.secondimg}></div>
+            </div>
+            <p>
+              According to local lore, Dayara Bugyal was once a sacred retreat
+              for ancient sages.
+            </p>
+          </div>
+        </div>
+        <div className={styles.dayBox}>
+          <h2>Day a to b on the trek</h2>
+          <p>
+            According to local lore, Dayara Bugyal was once a sacred retreat for
+            ancient sages. They meditated in these serene meadows, believing the
+            high-altitude silence brought them closer to enlightenment.{" "}
+          </p>
+          <div className={styles.imgWithDetail}>
+            <div className={styles.imgBox}>
+              <img className={style.firstimg} src={img2} />
+            </div>
+            <p>
+              According to local lore, Dayara Bugyal was once a sacred retreat
+              for ancient sages.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.checklistSection}>
+        <div className={styles.box}>
+          <p>
+            {" "}
+            Here’s a checklist of essentials to make sure you’re safe,
+            comfortable, and ready to soak in those Himalayan views:
+          </p>
+          <div className={styles.downloadDiv}>
+            <p>Download</p>
+            <button>Checklist</button>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.specialSection}>
+        <div className={styles.content}>
+          <h2>What Makes It Special</h2>
+          <ul>
+            <li>
+              <b>Meadows (Bugyals):</b> Vast alpine grasslands that feel almost
+              surreal{" "}
+            </li>
+            <li>
+              <b>Forests:</b> Walk through pine, oak, maple, and rhododendron
+              groves
+            </li>
+            <li>
+              <b>Panoramic Views:</b> Bandarpoonch, Kala Nag, Srikanth, and
+              Draupadi Ka Danda peaks
+            </li>
+            <li>
+              <b>All-Season Beauty:</b> Snow in winter, wildflowers in summer,
+              golden grass in autumn
+            </li>
+            <li>
+              <b>Peaceful Trails:</b> Unlike more crowded treks, Dayara offers
+              solitude and serenity, making it ideal for reflection and
+              reconnection with nature.
+            </li>
+            <li>
+              <b>Cultural Charm:</b> The base villages like Raithal and Barsu
+              offer a glimpse into Garhwali traditions, warm hospitality, and
+              even unique festivals like Butter Holi.
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section className={styles.VisitingTimeSection}>
+        <div className={styles.visitingCard}>
+          <h3>Best Time to Visit</h3>
+          <p>
+            Spring (Mar–Apr): Rhododendron blooms <br />
+            Summer (May–Jun): Lush green <br />
+            meadows Autumn (Sep–Nov): Clear skies, golden hues <br />
+            Winter (Dec–Feb): Snow-covered wonderland
+          </p>
+        </div>
+      </section>
+    </main>
   );
 }
 
